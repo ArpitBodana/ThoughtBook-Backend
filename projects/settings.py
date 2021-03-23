34b093c772ac02a3caaeda4fed7e88b885759d78
ArpitBodana <arpitbodana2@gmail.com>
2021-03-23 11:14:29 +0530

    more changes

--- projects/settings.py
+++ projects/settings.py
@@ -88,15 +88,19 @@ WSGI_APPLICATION = 'projects.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'Django_Rest',
-        'USER' : 'postgres',
-        'PASSWORD' : 1234,
-        'HOST':'localhost',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'Django_Rest',
+#         'USER' : 'postgres',
+#         'PASSWORD' : 1234,
+#         'HOST':'localhost',
+#     }
+# }
+
+import dj_database_url
+prod_db=dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
 
 
 # Password validation

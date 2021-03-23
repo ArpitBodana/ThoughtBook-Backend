bf0d0a5664d9c19d375b239a52eb4e50b2389dac
ArpitBodana <arpitbodana2@gmail.com>
2021-03-23 11:54:55 +0530

    ok

--- projects/settings.py
+++ projects/settings.py
@@ -100,7 +100,7 @@ WSGI_APPLICATION = 'projects.wsgi.application'
 
 import dj_database_url
 prod_db=dj_database_url.config(conn_max_age=500)
-DATABASE['default'].update(prod_db)
+DATABASES['default'].update(prod_db)
 
 
 # Password validation
